727de617d189c1c8bb628f32aab340a7650661d9
Abhay818 <101317592+Abhay818@users.noreply.github.com>
2022-03-11 10:55:06 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,16 @@
+public class calculator {
+    public static void main(String[] args){
+        int num1=7, num2=5, sum, multiply, divide, subt;
+
+        sum= num1+num2;
+        multiply=num1*num2;
+        divide=num1/num2;
+        subt=num1-num2;
+
+        System.out.println("sum="+sum);
+        System.out.println("multiply="+multiply);
+        System.out.println("divide="+divide);
+        System.out.println("subt="+subt);
+
+    }
+}
